649ad5d14a7582ad7da8d8370a81a19a58c563bb
Vincent Capol <vincent.capol@gmail.com>
2022-11-06 09:35:20 -0500

    weighted API calls

--- App/apicall.js
+++ App/apicall.js
@@ -1,9 +1,10 @@
-async function query(data) {
+//NLP model call
+async function queryNLP(data) {
     const response = await fetch(
         "https://api-inference.huggingface.co/models/j-hartmann/emotion-english-distilroberta-base",
         {
             headers: {
-                Authorization: "Bearer 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",
+                Authorization: "Bearer XYZ",
                 'Content-Type': 'application/json',
             },
             method: "POST",
@@ -13,17 +14,63 @@ async function query(data) {
     const result = await response.json();
     return result;
 }
+//Voice classification model call
+async function queryVoiceClassification(voiceData) {
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/harshit345/xlsr-wav2vec-speech-emotion-recognition",
+        {
+            headers: {
+                Authorization: "Bearer XYZ",
+                'Content-Type': 'application/json',
+            },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
+}
+async function weightOutputs(NLP, VC) {
+    let arr = [];
+    let maxLabel = "";
+    let maxValue = -1;
+    const finalOutput = "";
+    for (let i = 0; i < NLP.length; i++) {
+        for (let j = 0; j < NLP.length; j++) {
+            if (VC[i][j]['value'] > 0.8) {
+                return VC[i][j]['label']
+            }
+            arr[i] = VC[0][j]['value'] * 0.3 + NLP[0][j]['value'] * 0.7;
+            if (arr[i] > maxValue) {
+                maxLabel = VC[0][j]['label'];
+            }
+        }
+
+    }
+    return maxLabel;
+
+}
 let output = ""
-async function callQuery(inputVal) {
+async function callQuery(inputVal, voice) {
     console.log(inputVal)
-    query({ "inputs": inputVal }).then((response) => {
+    let outputNLP;
+    let outputVoice;
+    queryNLP({ "inputs": inputVal }).then((response) => {
         // console.log(JSON.stringify(response));
         // response = JSON.stringify(response);
         //the response we want!!!
         // console.log(response[0][0]['label'])
-        output = response[0][0]['label'];
-        console.log("o" + output);
-        return response;
+        // output = response[0][0]['label'];
+        // console.log("o" + output);
+        // return response;
+        outputNLP = response;
     });
+    queryVoiceClassification({ "inputs": voice }).then((response) => {
+        // output = response[0][0]['label'];
+        // console.log("o" + output);
+        // return response;
+        outputVoice = response;
+    });
+    output = weightOutputs(outputNLP, outputVoice);
 }
 export { callQuery, output };
